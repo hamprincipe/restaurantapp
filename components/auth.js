74f324ebbe798bddd7abc87fc58676680840d292
Ham Principe <hannahprincipe94@gmail.com>
2021-11-27 02:11:17 -0500

    remove hardcoded api url and remove backend folder

--- components/auth.js
+++ components/auth.js
@@ -5,7 +5,7 @@ import Router from "next/router";
 import Cookie from "js-cookie";
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://hamprincipe-capstoneproj.herokuapp.com/";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 //register a new user
 export const registerUser = (username, email, password) => {
--- components/restaurantList.js
+++ components/restaurantList.js
@@ -56,7 +56,7 @@ if(searchQuery.length > 0){
           top={true}
           style={{ height: 200 }}
           src={
-          `http://localhost:1337`+ res.image.url
+            res.image?.url ? process.env.NEXT_PUBLIC_API_URL + res.image.url : ''
           }
         />
         <CardBody>
